e9e4fc6140b0e0785f856f0da741e4984ae4c6e1
Bhoomi <bhoomis1020@gmail.com>
2022-12-16 23:16:49 +0530

    today's updates

---
+++ InfiniteArray.java
@@ -0,0 +1,50 @@
+package com.bhoomi;
+
+public class InfiniteArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170 };
+        int target = 10;
+
+        System.out.println(findingRange(arr, target));
+
+    }
+
+    static int findingRange(int[] arr, int target){
+
+        //first find range
+        //starting with size 2;
+
+        int start = 0;
+        int end = 1;
+
+        while(target > arr[end]){
+            int temp = end+1;
+            // new end = previous end + box_size*2
+            int newEnd = end + (end - start + 1)*2;
+            start = temp;
+        }
+
+        return BinarySearch(arr, target, start, end);
+    }
+
+
+    static int BinarySearch(int[] arr, int target, int start, int end) {
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+
+                start = mid + 1;
+            } else return mid;
+
+        }
+
+        return -1;
+    }
+}
