c3a5ee3d6f9ecec482d4508934d4854f3e94eee9
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-01-30 11:54:47 +0530

    day30

---
+++ PatternQs.java
@@ -0,0 +1,39 @@
+package com.bhoomi;
+
+public class PatternQs {
+    public static void main(String[] args) {
+
+        pattern6(5);
+        pattern7(5);
+
+    }
+
+    static void pattern6(int n){
+        //rows
+        for(int i = 1; i <= n; i++){
+            int spaces = n - i;
+                for(int k = 1; k <= spaces; k++){
+                    System.out.print(" ");
+                }
+            //cols
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern7(int n){
+        for(int i = 1; i<=n ; i++){
+            int cols = n + 1 -i;
+            int spaces = n - cols;
+            for(int k = 1; k <= spaces; k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<= cols; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
