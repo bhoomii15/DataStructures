2e9f98f6c886328f54368505b9c9435ad5230dfd
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-03-06 23:24:24 +0530

    day65

---
+++ CountZerosRecursive.java
@@ -0,0 +1,24 @@
+package com.bhoomi;
+
+public class CountZerosRecursive {
+    public static void main(String[] args) {
+        System.out.println(count(20405280));
+    }
+
+    static int count(int n){
+        return helper(n, 0);
+    }
+
+    private static int helper(int n, int c){
+        if(n == 0){
+            return c;
+        }
+
+        int rem = n % 10;
+        if(rem == 0){
+            return helper(n/10, c+1);
+        }
+        return helper(n/10, c);
+    }
+
+}
