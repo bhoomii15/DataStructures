5c329513cb71b876d4ac0d81f49adf66040bffa6
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-06-30 08:18:22 +0530

    DSUpdates

---
+++ stacksqueues/implementStacks.java
@@ -0,0 +1,21 @@
+package com.bhoomi.stacksqueues;
+
+import java.util.Stack;
+
+public class implementStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(3);
+        stack.push(4);
+        stack.push(56);
+        stack.push(44);
+        stack.push(99);
+
+        System.out.println(stack.pop());
+
+
+
+
+    }
+}
