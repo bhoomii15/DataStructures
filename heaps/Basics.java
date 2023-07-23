f36128534168663178d01c140e248f598aed2eaa
Bhoomi Sharma <bhoomis1020@gmail.com>
2023-07-23 22:24:11 +0530

    DSAUpdates

---
+++ heaps/Basics.java
@@ -0,0 +1,20 @@
+package com.bhoomi.heaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Basics {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+    }
+}
